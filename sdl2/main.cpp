facba66cd3b8beb5a45449b9d75dc655c786d31b
ottav14 <dominicjottaviano@gmail.com>
2024-04-16 22:24:27 -0400

    added to sdl template

--- sdl2/main.cpp
+++ sdl2/main.cpp
@@ -3,34 +3,41 @@
 
 int main() {
 
+  // Initialize framework
   SDL_Window* window = nullptr;
   SDL_Renderer* renderer = nullptr;
-  SDL_Event e;
-
   SDL_Init(SDL_INIT_EVERYTHING);
-
   SDL_CreateWindowAndRenderer(1920, 1080, 0, &window, &renderer);
 
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-  // Draw a red rectangle
-  SDL_Rect rect = {100, 100, 200, 150};
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red color
-  SDL_RenderFillRect(renderer, &rect);
+  // Initialize variables
+  double time = 0.0;
 
-  // Update the window
-  SDL_RenderPresent(renderer);
-
-  // Main loop: handle events
+  // Main loop
   bool quit = false;
   SDL_Event event;
-  while (!quit) {
-    while (SDL_PollEvent(&event)) {
+  while(!quit) {
+    while(SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
 	quit = true;
       }
     }
+
+    // Draw background
+    SDL_Rect bg = {0, 0, 1920, 1080};
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); 
+    SDL_RenderFillRect(renderer, &bg);
+
+    // Draw a red rectangle
+    SDL_Rect rect = {100 + (int) time, 100, 200, 150};
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); 
+    SDL_RenderFillRect(renderer, &rect);
+
+    // Update the window
+    SDL_RenderPresent(renderer);
+
+    // Update time
+    time += 0.1;
   }
 
   // Clean up resources
@@ -38,8 +45,7 @@ int main() {
   SDL_DestroyWindow(window);
   SDL_Quit();
 
-
-  std::cout << "Ran" << std::endl;
+  std::cout << "Ran successfully!" << std::endl;
   return 0;
 
 }
